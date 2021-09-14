b946bec5a1a3261e70b5133655920510b1a69e57
Korotkova-Iuliia <3589900@gmail.com>
2021-09-14 10:18:38 +0300

    task 5

--- js/index.js
+++ js/index.js
@@ -1,36 +1,286 @@
-//
+// 1
+// Создание объекта
+// Объекты позволяют описать и сгруппировать характеристики объектов реального мира - пользователя, книги, продукта магазина, чего угодно. Объекты ещё называют словарями, то есть они содержат термины (свойства) и их определения (значения).
 
-// ✅ Логически и синтаксически сгруппированные сущности
-const bookShelf = {
-  books: ['The Last Kingdom', 'The Last Kingdom', 'Dream Guardian'],
-  getBooks() {
-    // console.log('Этот метод будет возвращать все книги - свойство books');
-    console.log(this);
-  },
-  papers: ['The Last Kingdom', 'Dream Guardian'],
-  getBooks() {
-    // console.log('Этот метод будет возвращать все книги - свойство books');
-    console.log(this);
-  },
-  addBook(bookName) {
-    bookShelf.books.push('драма');
-    console.log(this);
-    // console.log(bookName);
-  },
-  addBookNew(bookName) {
-    // bookShelf.books.push('yes');
-    console.log(this);
-    // console.log(bookName);
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+// Для объявления используются фигурные скобки {} - литерал объекта. При создании объекту можно добавить свойства, каждое из которых описывается парами ключ:значение. Ключ ещё называют именем свойства и это всегда строка. Значением свойства могут быть любые типы: примитивы, массивы, объекты, були, функции и т. п. Свойства разделяются запятой.
+
+// Задание
+// Присвой переменной apartment объект описывающий квартиру со следующими характеристиками:
+
+// imgUrl - фотография, значение "https://via.placeholder.com/640x480";
+// descr - описание, значение "Spacious apartment in the city center";
+// rating - рейтинг, значение 4;
+// price - цена, значение 2153;
+// tags - метаинформация, массив ["premium", "promoted", "top"].
+// Тесты
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// В объекте есть свойство imgUrl
+// Значение свойства imgUrl это строка "https://via.placeholder.com/640x480"
+// В объекте есть свойство descr
+// Значение свойства descr это строка "Spacious apartment in the city center"
+// В объекте есть свойство rating
+// Значение свойства rating это число 4
+// В объекте есть свойство price
+// Значение свойства price это число 2153
+// В объекте есть свойство tags
+// Значение свойства tags это массив ["premium", "promoted", "top"]
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// 2
+// Вложенные свойства
+// Значением свойства может быть другой объект. Это используется для хранения вложенных и группированных данных.
+
+// Например, статистика пользователя социальной сети состоит из количества последователей, просмотров и лайков, и хранить эти данные удобнее всего в виде объекта. Тоже самое с местоположением, отдельно страна и город.
+
+// В будущем это можно будет использовать для поиска пользователей по стране, городу, минимальному или максимальному количеству последователей и т. д.
+
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: {
+//     country: "Jamaica",
+//     city: "Ocho Rios",
+//   },
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+// Задание
+// Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
+
+// name - имя владельца, значение "Henry";
+// phone - телефон, значение "982-126-1588";
+// email - почта, значение "henry.carter@aptmail.com".
+// Тесты
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// У объекта переменной apartment присутствуют свойства imgUrl, descr, rating, price и tag со значениями
+// В объекте apartment есть свойство owner
+// Значение свойства owner это объект
+// В объекте owner есть свойство name
+// Значение свойства name это "Henry"
+// В объекте owner есть свойство phone
+// Значение свойства phone это "982-126-1588"
+// В объекте owner есть свойствао email
+// Значение свойства email это "henry.carter@aptmail.com"
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
   },
 };
 
-// console.log(bookShelf.books);
+// 3
+// Доступ к свойствам через точку
+// Первый способ получить доступ к свойству объекта это синтаксис обьект.ключ_свойства. Синтаксис «через точку» используется в большинстве случаев и подходит тогда, когда мы заранее знаем имя (ключ) свойства к которому хотим получить доступ.
 
-// Вызовы методов
-// bookShelf.getBooks();
-// bookShelf.addBook();
-bookShelf.getBooks();
-bookShelf.addBook();
-bookShelf.addBookNew();
+// На место обращения будет возвращено значение свойства с таким именем.
+// Если в объекте нет свойства с таким именем, на место обращения вернётся undefined.
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
 
-// bookShelf.getBooks();
+// const bookTitle = book.title;
+// console.log(bookTitle); // "The Last Kingdom"
+
+// const bookGenres = book.genres;
+// console.log(bookGenres); // ["historical prose", "adventure"]
+
+// const bookPrice = book.price;
+// console.log(bookPrice); // undefined
+// Задание
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+// Тесты
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// У объекта переменной apartment присутствуют свойства imgUrl, descr, rating, price и tag со значениями
+// Объявлена переменная aptRating
+// Значение переменной aptRating это число 4
+// Объявлена переменная aptDescr
+// Значение переменной aptDescr это строка "Spacious apartment in the city center"
+// Объявлена переменная aptPrice
+// Значение переменной aptPrice это число 2153
+// Объявлена переменная aptTags
+// Значение переменной aptTags это массив строк ["premium", "promoted", "top"]
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// Change code below this line
+const aptRating = apartment.rating;
+const aptDescr = apartment.descr;
+const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+// Change code above this line
+
+// 4
+// Доступ к вложенным свойствам
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: {
+//     country: "Jamaica",
+//     city: "Ocho Rios",
+//   },
+//   hobbies: ["swiming", "music", "sci-fi"],
+// };
+// Для доступа к вложенным свойствам используется цепочка обращений «через точку». Например, если необходимо получить значение страны пользователя, записываем user.location.country, где user.location это обращение (путь) к объекту в свойстве location, а user.locaton.country обращение к свойству country в этом объекте. То есть, «точка» указывает следующую вложенность.
+
+// const location = user.location;
+// console.log(location); // Объект location
+
+// const country = user.location.country;
+// console.log(country); // "Jamaica"
+// Если значение свойства это массив, то в нашем примере user.hobbies - обращение к этому массиву. Далее, можно получить доступ к его элементам через квадратные скобки и индекс или использовать свойства и методы.
+
+// const hobbies = user.hobbies;
+// console.log(hobbies); // ["swiming", "music", "sci-fi"]
+
+// const firstHobby = user.hobbies[0];
+// console.log(firstHobby); // "swiming"
+
+// const numberOfHobbies = user.hobbies.length;
+// console.log(numberOfHobbies); // 3
+// Задание
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// ownerName - имя владельца;
+// ownerPhone - телефон владельца;
+// ownerEmail - почта владельца;
+// numberOfTags - количество элементов массива в свойстве tags;
+// firstTag - первый элемент массива в свойстве tags;
+// lastTag - последний элемент массива в свойстве tags.
+// Тесты
+// Объявлена переменная apartment с помощью const
+// Значение переменной apartment это объект
+// Объявлена переменная ownerName с помощью const
+// Значение переменной ownerName это строка "Henry"
+// Объявлена переменная ownerPhone с помощью const
+// Значение переменной ownerPhone это "982-126-1588"
+// Объявлена переменная ownerEmail с помощью const
+// Значение переменной ownerEmail это "henry.carter@aptmail.com"
+// Объявлена переменная numberOfTags с помощью const
+// Значение переменной numberOfTags это 3
+// Объявлена переменная firstTag с помощью const
+// Значение переменной firstTag это "premium"
+// Объявлена переменная lastTag с помощью const
+// Значение переменной lastTag это "top"
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[numberOfTags - 1];
+// Change code above this line
+
+// 5
+// Доступ к свойствам через квадратные скобки
+// Второй способ получить доступ к свойству объекта это синтаксис обьект["ключ_свойства"]. Похоже на обращение к элементу массива с отличием в том, что в скобках указывается не индекс элемента, а имя свойства как строка.
+
+// Синтаксис «квадратных скобок» используется значительно реже. Как правило в случаях когда имя свойства заранее неизвестно или оно хранится в переменной (как значение параметра функции, например).
+
+// На место обращения будет возвращено значение свойства с таким именем.
+// Если в объекте нет свойства с таким именем, на место обращения вернётся undefined.
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// const bookTitle = book["title"];
+// console.log(bookTitle); // "The Last Kingdom"
+
+// const bookGenres = book["genres"];
+// console.log(bookGenres); // ["historical prose", "adventure"]
+
+// const propKey = "author";
+// const bookAuthor = book[propKey];
+// console.log(bookAuthor); // "Bernard Cornwell"
+// Задание
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+// Тесты
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// Объявлена переменная aptRating
+// Значение переменной aptRating это 4
+// Объявлена переменная aptDescr
+// Значение переменной aptDescr это "Spacious apartment in the city center"
+// Объявлена переменная aptPrice
+// Значение переменной aptPrice это 2153
+// Объявлена переменная aptTags
+// Значение переменной aptTags это ["premium", "promoted", "top"]
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// Change code below this line
+const aptRating = apartment['rating'];
+const aptDescr = apartment['descr'];
+const aptPrice = apartment['price'];
+const aptTags = apartment['tags'];
+// Change code above this line
